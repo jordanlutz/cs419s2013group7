79eabaae07ac9e7edb026c18f89c7393c411d858
AndrewMBeverly@gmail.com <AndrewMBeverly@gmail.com@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-04-23 03:35:15 +0000

    connection script

---
+++ branches/PHP/sqliConnect.php
@@ -0,0 +1,7 @@
+    <?php 
+    $mysqli = new mysqli("mysql.cs.orst.edu", "cs419_beverlya", "fdqNpFaqnfmzKaGK", "cs419_beverlya");
+    if ($mysqli->connect_errno) {
+    	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+	
+	?>
