118945b228bd637e899b595488492859ea13b5ad
AndrewMBeverly@gmail.com <AndrewMBeverly@gmail.com@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-04-23 04:21:59 +0000

    updated the database domain

--- branches/PHP/sqliConnect.php
+++ branches/PHP/sqliConnect.php
@@ -1,5 +1,5 @@
     <?php 
-    $mysqli = new mysqli("mysql.cs.orst.edu", "cs419_beverlya", "fdqNpFaqnfmzKaGK", "cs419_beverlya");
+    $mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419_beverlya", "fdqNpFaqnfmzKaGK", "cs419_beverlya");
     if ($mysqli->connect_errno) {
     	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
