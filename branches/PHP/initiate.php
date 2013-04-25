0710ca4ce8c9f766f202b1670920fde9b441077d
AndrewMBeverly@gmail.com <AndrewMBeverly@gmail.com@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-04-25 05:06:02 +0000

    updated

--- branches/PHP/initiate.php
+++ branches/PHP/initiate.php
@@ -1,18 +1,19 @@
-    <?php 
+  <?php 
 	
 	include "sqliConnect.php";
 	
 	connect();
 	printf ( "Successfully connected to database!\n");
+	$sql = "create table ACCESSPOINT( 
+		pointID int not null auto_increment, 
+		BSSID varchar(255) not null, 
+		latitude Double not null, 
+		longitude Double not null, 
+		altitude Double not null, 
+		additionalInfo varchar(255) not null, 
+		primary key (pointID))";
 	
-	if($mysqli->query('create table ACCESSPOINT( 
-	pointID int not null auto_increment, 
-	BSSID varchar(255) not null, 
-	latitude Double not null, 
-	longitude Double not null, 
-	altitude Double not null, 
-	additionalInfo varchar(255) not null, 
-	primary key (pointID))') === TRUE){
+	if( $mysqli->query($sql) === TRUE){
 		printf("Table Created\n");
 	}
 	else{
