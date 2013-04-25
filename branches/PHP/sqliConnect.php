f08c523ca8518c1d730bb02c6bb51f8a1df77992
AndrewMBeverly@gmail.com <AndrewMBeverly@gmail.com@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-04-25 04:33:48 +0000

    update connect function

--- branches/PHP/sqliConnect.php
+++ branches/PHP/sqliConnect.php
@@ -1,7 +1,12 @@
-    <?php 
-    $mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419_beverlya", "fdqNpFaqnfmzKaGK", "cs419_beverlya");
+  <?php 
+  /****
+  * Function creates a connect to database.
+  ****/
+  function connect(){
+    $mysqli = new mysqli("mysql.cs.orst.edu", "cs419_beverlya", "fdqNpFaqnfmzKaGK", "cs419_beverlya");
     if ($mysqli->connect_errno) {
-    	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+      echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+      exit();
     }
-	
-	?>
+  }
+  ?>
