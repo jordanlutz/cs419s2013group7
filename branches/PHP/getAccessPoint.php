4ded59812730484d145ace92d63fdec9d46d31d2
jordanlutz@gmail.com <jordanlutz@gmail.com@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-04-26 19:02:07 +0000

    Updated getAccessPoint to create JSON object

--- branches/PHP/getAccessPoint.php
+++ branches/PHP/getAccessPoint.php
@@ -1,35 +1,22 @@
-    <?php 
-	
+<?php 
 	include "sqliConnect.php";
 	
 	$mysqli = connect();
-	echo "Successfully connected to database!";
-	echo "<br />";
+		
 	$sql = "SELECT * FROM ACCESSPOINT";
 
 	$result = $mysqli->query($sql);
 
-	while($row = $result->fetch_array()) {
-  		echo "<br />";	
-		echo $row['pointID'];
- 		echo "<br />";
-		echo $row['BSSID'];
- 		echo "<br />";
-		echo $row['latitude'];
- 		echo "<br />";
-		echo $row['longitude'];
- 		echo "<br />";
-		echo $row['altitude'];
- 		echo "<br />";
-		echo $row['additionalInfo'];
- 		echo "<br />";
- 		echo "<br />";
-  	}
+	$rows = array();
 
+	while($r = mysqli_fetch_assoc($result)) {
+		$rows[] = $r;
+	}
+
+	$data = json_encode($rows);
+	
+	print $data;
+	
 	$mysqli->close();
 
-	echo "<br />";
-	echo "Connection closed!";
-	echo "<br />";
-	
-	?>
+?>
