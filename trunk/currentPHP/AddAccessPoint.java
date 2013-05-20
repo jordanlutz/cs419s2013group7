3a072ac6c0be7bcec8c0a7a1c2399819e006b236
cooneyj.workemail@gmail.com <cooneyj.workemail@gmail.com@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-05-20 02:42:11 +0000

    Java files loaded

---
+++ trunk/currentPHP/AddAccessPoint.java
@@ -0,0 +1,20 @@
+package com.example.projectinterface;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class AddAccessPoint extends Activity implements OnClickListener {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addaccesspoint);
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+	}
+}
