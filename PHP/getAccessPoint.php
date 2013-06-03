23d67b05a3e3b6e69533826c987ed6015fc1111b
jordanlutz <jordanlutz@299bb4ec-61aa-b464-3d0f-1842c7e773c1>
2013-06-03 03:19:53 +0000

    Updated getAccessPoint.php and triangulator.php

--- PHP/getAccessPoint.php
+++ PHP/getAccessPoint.php
@@ -1,18 +1,30 @@
 <?php 
-	//echo "test";
 	include "sqliConnect.php";
-	function getAccessPoint($BSSID){
+
+	function getAccessPoint($BSSID) {
 		$mysqli = connect();
 			
-		$sql = "SELECT * FROM ACCESSPOINT WHERE BSSID=".$BSSID."";
+		$sql = "SELECT * FROM ACCESSPOINT WHERE BSSID='$BSSID'";
 
 		$result = $mysqli->query($sql);
 
 		$data = mysqli_fetch_assoc($result);
 			
+		$mysqli->close();	
+	
 		return $data;
-		
+	}
+
+	function insertAccessPoint($data) {
+		$mysqli = connect();
+			
+		$sql = "INSERT INTO ACCESSPOINT (BSSID) VALUES ('".$data['BSSID']."')";
+
+		$result = $mysqli->query($sql);
+	
 		$mysqli->close();
+
+		return $result;
 	}
 
 ?>
